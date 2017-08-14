20961fa9afedfa408f53ed249db9700e52527e4d
Mayat Smolnik <mcsmolnik@gmail.com>
2017-08-14 11:42:37 -0600

    more

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -14,15 +14,26 @@ module.exports = function(app) {
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
+  	//staticRoutes(app)
 
-  
+
+
 
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
 
-  // If no matching route is found default to home
-  app.use(function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
+
+  app.get("/public/:folder/:file", function(req, res){
+    var folder=req.params.folder;
+    var file=req.params.file;
+    res.sendFile(path.join(__dirname, "/../public",folder,file));
+  });
+  // If no matching route is found default to home
+  // app.use(function(req, res) {
+  //   res.sendFile(path.join(__dirname, "/../public/home.html"));
+  // });
 };
